17213d7f60cb97228a5e27387c334283430d79bb
timebug <timebug.info@gmail.com>
2013-06-23 17:47:01 +0800

    v2.1.0 release. distribute as an python egg; closed #1

--- upyun/__init__.py
+++ upyun/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'upyun'
-__version__ = '2.0.3'
+__version__ = '2.1.0'
 __author__ = 'Monkey Zhang (timebug)'
 __license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'
 __copyright__ = 'Copyright 2013 UpYun.com'
